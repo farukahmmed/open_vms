fc9cfbef938b624934d73940fbe323dcf0025be1
farukahmmed <faruk.fa@gmail.com>
2022-07-16 17:20:07 +0600

    new doctype added

---
+++ open_vms/open_vms/doctype/vms_fleet_types/test_vms_fleet_types.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, faruk and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testvms_Fleet_Types(FrappeTestCase):
+	pass
