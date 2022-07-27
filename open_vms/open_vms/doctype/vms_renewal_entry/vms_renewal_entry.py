12c51ff4ee1a822a1699b43bb57555f01c2f10a2
farukahmmed <faruk.fa@gmail.com>
2022-07-27 11:35:36 +0600

    ENTRY FORM CREATED

---
+++ open_vms/open_vms/doctype/vms_renewal_entry/vms_renewal_entry.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, faruk and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class vms_Renewal_Entry(Document):
+	pass
