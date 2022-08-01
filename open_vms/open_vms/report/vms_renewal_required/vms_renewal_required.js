dcb0d4ede7cf19ee6b25808c3b65ea7f9045b822
farukahmmed <faruk.fa@gmail.com>
2022-08-01 18:45:14 +0600

    New report: vms_renewal_entry

---
+++ open_vms/open_vms/report/vms_renewal_required/vms_renewal_required.js
@@ -0,0 +1,25 @@
+frappe.query_reports["vms_Renewal_Required"] = {
+    "filters": [
+    
+        {
+            fieldname:"from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            width: "80",
+            default: frappe.datetime.month_start(),
+            reqd: 1
+                    },
+
+                    
+    {
+            fieldname:"to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            width: "80",
+            default: frappe.datetime.month_end(),
+            reqd: 1
+                                }
+       
+    ]
+    
+    }
