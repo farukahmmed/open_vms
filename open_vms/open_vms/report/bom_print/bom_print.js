b2bc41065215614c2497a27011bc40a95f325148
farukahmmed <faruk.fa@gmail.com>
2022-10-21 10:19:59 +0600

    Vehicle allocation bom_for_customer

---
+++ open_vms/open_vms/report/bom_print/bom_print.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2022, faruk and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["BOM Print"] = {
+	//"filters": [
+
+	//]
+
+	//frappe.query_reports["Accounts Receivable"] = {
+		"filters": [
+			
+		]
+	//}
+};
---
+++ open_vms/open_vms/report/bom_print/bom_print.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022, faruk and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+
+def execute(filters=None):
+	columns, data = [], []
+	
