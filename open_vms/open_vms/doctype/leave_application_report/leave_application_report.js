25c931003e64e34bc9075207ecf37289babe6db6
farukahmmed <faruk.fa@gmail.com>
2023-01-02 16:22:41 +0600

    rdl report update

--- open_vms/open_vms/doctype/leave_application_report/leave_application_report.js
+++ open_vms/open_vms/doctype/leave_application_report/leave_application_report.js
@@ -3,7 +3,13 @@
 
 frappe.ui.form.on('Leave Application Report', {
 	 refresh: function(frm) {
-		window.open("http://report.panamgroupbd.com:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fpcil+Leave+Application&rs:Command=Render", "_blank", "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1200,height=550"); 
-	
-	 }
+		window.open("http://report.panamgroupbd.com:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fpcil+Leave+Application&rs:Command=Render",
+		 "_blank", "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1200,height=550"); 
+		history.back()
+
+
+}
+
+	 
+
 });
