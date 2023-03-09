612c6d9025c147c4ff2c262381589a3c59dd1ce3
farukahmmed <faruk.fa@gmail.com>
2023-03-09 18:21:04 +0600

    Rdl Salary report

---
+++ open_vms/open_vms/doctype/salary_sheet_new_employee/salary_sheet_new_employee.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2023, faruk and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Salary Sheet-New Employee', {
+	refresh: function(frm) {
+		window.open("http://report.panamgroupbd.com:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fPCIL+Salary+Sheet-New+Employee&rs:Command=Render",
+		 "_blank", "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1200,height=550"); 
+		history.back()
+
+
+}
+
+});
