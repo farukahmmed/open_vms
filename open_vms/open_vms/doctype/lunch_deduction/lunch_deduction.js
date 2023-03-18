abbfed27eb5881436371ab54d80fccd0cc55c19c
farukahmmed <faruk.fa@gmail.com>
2023-03-18 16:22:26 +0600

    loan and advance updted

--- open_vms/open_vms/doctype/lunch_deduction/lunch_deduction.js
+++ open_vms/open_vms/doctype/lunch_deduction/lunch_deduction.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Lunch Deduction', {
-	// refresh: function(frm) {
+	before_save: function(frm) {
 
-	// }
+			var dDate=new Date(frm.doc.date)
+			var dMonth=dDate.getMonth()+1
+			var dYear=dDate.getFullYear()
+			//frappe.msgprint(__("value is '{0}'", [dDate]))
+			//frappe.msgprint(__("value is '{0}'", [dMonth]))
+			//frappe.msgprint(__("value is '{0}'", [dYear]))
+
+			var id_field = frm.doc.employee + "-" + dMonth + "-" + dYear// get the value of the id field
+			frm.set_value('unique_id', id_field); // set the value of the unique id field
+			
+		}
 });
