354323505869fe4f3c57593d9018327daac29021
farukahmmed <faruk.fa@gmail.com>
2023-03-19 10:01:32 +0600

    deduction entry

---
+++ open_vms/open_vms/doctype/advance_deduction_entry/advance_deduction_entry.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2023, faruk and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Advance Deduction Entry', {
+	validate: function(frm) {
+
+		var dDate=new Date(frm.doc.date)
+		var dMonth=dDate.getMonth()+1
+		var dYear=dDate.getFullYear()
+		//frappe.msgprint(__("value is '{0}'", [dDate]))
+		//frappe.msgprint(__("value is '{0}'", [dMonth]))
+		//frappe.msgprint(__("value is '{0}'", [dYear]))
+
+		var id_field = frm.doc.employee + "-" + dMonth + "-" + dYear// get the value of the id field
+		frm.set_value('unique_id', id_field); // set the value of the unique id field
+		
+	}
+});
