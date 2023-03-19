894d08c1cea79878320cda5eb71c46ae0c495000
farukahmmed <faruk.fa@gmail.com>
2023-03-19 13:40:26 +0600

    deleted doctype

--- open_vms/open_vms/doctype/lunch_deduction/lunch_deduction.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, faruk and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class LunchDeduction(Document):
-	pass
