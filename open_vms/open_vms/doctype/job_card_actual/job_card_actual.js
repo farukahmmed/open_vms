15fb6773136aebb5f0c7c8ee67761726ff831d8f
farukahmmed <faruk.fa@gmail.com>
2023-03-30 12:01:59 +0600

    new reports added

--- open_vms/open_vms/doctype/job_card_actual/job_card_actual.js
+++ open_vms/open_vms/doctype/job_card_actual/job_card_actual.js
@@ -3,7 +3,7 @@
 
 frappe.ui.form.on('Job_Card_Actual', {
 	refresh: function(frm) {
-		window.open("http://172.16.16.14:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fPCIL+Job+Card+Actual&rs:Command=Render",
+		window.open("http://report.panamgroupbd.com:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fPCIL+Job+Card+Actual&rs:Command=Render",
 		 "_blank", "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1200,height=550"); 
 		history.back()
 	}
--- open_vms/open_vms/doctype/salary_sheet_with_ot/salary_sheet_with_ot.js
+++ open_vms/open_vms/doctype/salary_sheet_with_ot/salary_sheet_with_ot.js
@@ -1,9 +1,9 @@
 // Copyright (c) 2023, faruk and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Job_Card', {
+frappe.ui.form.on('Salary Sheet-With OT', {
 	refresh: function(frm) {
-		window.open("http://report.panamgroupbd.com:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fPCIL+Job+Card&rs:Command=Render",
+		window.open("http://report.panamgroupbd.com:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fPCIL+Salary+Sheet-With+2Hr+OT&rs:Command=Render",
 		 "_blank", "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1200,height=550"); 
 		history.back()
 }
