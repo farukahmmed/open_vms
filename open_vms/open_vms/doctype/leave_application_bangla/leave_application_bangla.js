33c36fd95b193db775265fff68c60802f2980160
farukahmmed <faruk.fa@gmail.com>
2023-05-08 16:05:06 +0600

    reports deployed

---
+++ open_vms/open_vms/doctype/leave_application_bangla/leave_application_bangla.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2023, faruk and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Leave Application Bangla', {
+	refresh: function(frm) {
+		window.open("http://report.panamgroupbd.com:8081/rsvd/Pages/ReportViewer.aspx?%2fREPORTS%2fPANAM+CYCLE%2ferp%2fPCIL+Leave+Application+Bangla&rs:Command=Render",
+		 "_blank", "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1200,height=550"); 
+		history.back()
+	}
+});
