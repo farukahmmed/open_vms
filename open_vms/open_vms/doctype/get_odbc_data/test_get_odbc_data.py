e681827ced703a61a403731c7ee300e74be6b30f
farukahmmed <faruk.fa@gmail.com>
2024-01-26 11:22:06 +0600

    get_odbc_data moved to shop app

--- open_vms/open_vms/doctype/get_odbc_data/test_get_odbc_data.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, faruk and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testget_odbc_data(FrappeTestCase):
-	pass
