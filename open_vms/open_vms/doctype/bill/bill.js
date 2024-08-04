cbff81cb101d394fe7f4bb0efdd038d5af28d3e3
farukahmmed <faruk.fa@gmail.com>
2024-08-04 17:26:08 +0600

    bill updated

--- open_vms/open_vms/doctype/bill/bill.js
+++ open_vms/open_vms/doctype/bill/bill.js
@@ -1,8 +1,40 @@
 // Copyright (c) 2024, faruk and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bill", {
-// 	refresh(frm) {
+frappe.ui.form.on("Bill", {
+	refresh:function(frm) {
+        calculate_total_amount(frm);       
 
-// 	},
-// });
+	},
+    amount:function(frm){
+        calculate_total_amount(frm);
+
+    },
+    additional_amount:function(frm){
+        calculate_total_amount(frm)
+    }
+});
+
+//Function to calculate total amount and convert it to in-words.
+function calculate_total_amount(frm){
+    let amount=frm.doc.amount ||0;
+    let additional_amount=frm.doc.additional_amount || 0;
+    let total_amount= amount+additional_amount;
+    frm.set_value('total_amount',total_amount);
+
+    if (total_amount){
+        frappe.call({
+            method: 'open_vms.open_vms.doctype.bill.bill.get_total_amount_in_words',
+            args:{
+                amount: total_amount,
+                currency: frm.doc.currency || "BDT"
+            },
+            callback:function(r){
+                if (r.message){
+                    frm.set_value('in_words', r.message)
+                }
+            }
+        });
+    }
+
+}
--- open_vms/open_vms/doctype/bill/bill.py
+++ open_vms/open_vms/doctype/bill/bill.py
@@ -2,8 +2,17 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
+from frappe.utils import money_in_words
+
+
+
 from frappe.model.document import Document
 
 
 class Bill(Document):
 	pass
+
+@frappe.whitelist()
+def get_total_amount_in_words(amount, currency):
+    return money_in_words(amount, currency)
