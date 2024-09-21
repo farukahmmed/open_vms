4965aaedc654d754881ebf118d798d5f756f3db8
farukahmmed <faruk.fa@gmail.com>
2024-09-21 17:02:05 +0600

    textile_rnd_hanger_list

---
+++ open_vms/open_vms/doctype/textile_rnd_hanger_list/textile_rnd_hanger_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, faruk and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Textile RnD Hanger List", {
+// 	refresh(frm) {
+
+// 	},
+// });
