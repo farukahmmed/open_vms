be71c82e0cf6cb37ff0a57a94b5afae7eeda8f68
farukahmmed <faruk.fa@gmail.com>
2024-09-23 12:56:13 +0600

    script for textile_rnd_hanger QR Code Scanner

--- open_vms/open_vms/doctype/textile_rnd_hanger/test_textile_rnd_hanger.py
+++ open_vms/open_vms/doctype/textile_rnd_hanger/test_textile_rnd_hanger.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestTextileRnDHangerList(FrappeTestCase):
+class TestTextileRnDHanger(FrappeTestCase):
 	pass
--- open_vms/open_vms/doctype/textile_rnd_hanger/textile_rnd_hanger.py
+++ open_vms/open_vms/doctype/textile_rnd_hanger/textile_rnd_hanger.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class TextileRnDHangerList(Document):
+class TextileRnDHanger(Document):
 	pass
